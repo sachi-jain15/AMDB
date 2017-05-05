b8ee0a188be25ee6a0a6083ec36b635aa1c84fca
sachi-jain15 <sachijain15@gmail.com>
2017-05-05 14:52:23 +0530

    Final commit

--- AMDB_API/users/serializer.py
+++ AMDB_API/users/serializer.py
@@ -1,13 +1,14 @@
+# Import commands :-
 from rest_framework import serializers
 from users.models import Users, movies
 
-
+# to get user model serialized
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ('id', 'name', 'username', 'email', 'short_bio', 'created_on', 'updated_on')
 
-
+# to get movie model serialized
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = movies
