c84ca38488cbc199088c5c1e9b63da82bfd042c2
sachi-jain15 <sachijain15@gmail.com>
2017-05-05 13:10:14 +0530

    Access_token model added

--- AMDB_API/users/models.py
+++ AMDB_API/users/models.py
@@ -12,3 +12,14 @@ class Users(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+class access_token(models.Model):
+    user_id = models.ForeignKey(Users,on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_request_on = models.DateTimeField(auto_now=True)
+    is_valid = models.BooleanField(default=True)
+
+    def create_token(self):
+        self.access_token = uuid.uuid4()
+
+
